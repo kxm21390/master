2c3b563260cbc28a00f234d14038b8bdc820cdae
kxm21390 <44552588+kxm21390@users.noreply.github.com>
2018-10-31 14:11:36 -0700

    Update MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -5,6 +5,6 @@ public class MyFirstJavaProgram {
     */
 
    public static void main(String []args) {
-      System.out.println("Hello World"); // prints Hello World
+      System.out.println("Hello World Kiran"); // prints Hello World
    }
 }
